1dbbd3317087b77929c13d37488ece79267e5c26
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-26 17:10:28 +0300

    commit 2

---
+++ app/src/main/java/com/example/androidplayground/VM.kt
@@ -0,0 +1,17 @@
+package com.example.androidplayground
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class VM : ViewModel() {
+
+    init {
+
+        viewModelScope.launch {
+
+        }
+        
+    }
+
+}
