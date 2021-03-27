d9e29410c2a79adcf4ac41f56d4310defe2df8d9
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-27 22:08:26 +0300

    added utils file

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.androidplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bkCoroutineContext: CoroutineContext = Dispatchers.Default
+
+        val bkCoroutineScope: CoroutineScope = CoroutineScope(context = bkCoroutineContext)
+
+
+
+        val job: Job = bkCoroutineScope.launch(block = { -> })
+
     }
 }
---
+++ app/src/main/java/com/example/androidplayground/Utils.kt
@@ -0,0 +1,17 @@
+package com.example.androidplayground
+
+import android.util.Log
+
+fun printD(message: String) {
+    Log.d("**BK-DEBUG**", message)
+}
+
+fun printE(message: String) {
+    Log.e("**BK-DEBUG**", message)
+}
+
+fun printI(message: String) {
+    Log.i("**BK-DEBUG**", message)
+}
+
+
